ae2c56cc8d10f0fbe069b8cfbb376116712ee207
Luana Mennuti <luigina.mennuti2@studio.unibo.it>
2026-04-20 11:13:10 +0200

    feat: add Prank class with prank message

---
+++ src/main/java/Prank.java
@@ -0,0 +1,11 @@
+class Prank {
+
+    public static void main(String[] args) {
+        Prank prank = new Prank();
+        prank.isAPrank();
+    }
+
+    private void isAPrank() {
+        System.out.println("Is this a prank? Yes, it is!");
+    }
+}
