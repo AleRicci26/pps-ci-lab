59a8df0ee682d615a500ae53ff2db6fa06e7630d
Luana Mennuti <luigina.mennuti2@studio.unibo.it>
2026-04-20 17:45:17 +0200

    feat: configure testing dependencies and JUnit platform in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,3 +2,16 @@ plugins {
     java
     scala
 }
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter:6.0.3")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:6.0.3")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
