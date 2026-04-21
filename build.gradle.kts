d0f4342fbd88733ba903a931764e7b8251355125
Luana Mennuti <luigina.mennuti2@studio.unibo.it>
2026-04-21 13:33:20 +0200

    feat: add Scala library and testing dependencies to build configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,17 @@ repositories {
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:6.0.3")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher:6.0.3")
+    implementation("org.scala-lang:scala3-library_3:3.8.3")
+    testImplementation("org.scalatest:scalatest_3:3.2.20")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.withType<ScalaCompile>().configureEach {
+    scalaCompileOptions.additionalParameters = listOf(
+        "-Xunchecked-java-output-version",
+        JavaVersion.current().majorVersion,
+    )
 }
